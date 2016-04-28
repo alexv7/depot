a9f883b7e9c4e93ecc310313d4d46964d607d1ea
alexv7 <alexvarjabedian7@gmail.com>
2016-04-28 12:02:53 -0400

    Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+  # we used the allow_blank option to avoid getting multiple error messages (cant be blank, and format be must be gif, jpg, png)
 end
