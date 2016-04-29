3a3f6f99836aaa5e9ae83b4ec4d56ac5a0a84011
alexv7 <alexvarjabedian7@gmail.com>
2016-04-29 13:13:57 -0400

    Added cart button

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -3,7 +3,7 @@ module CartsHelper
   private
 
   def set_cart
-    @cart = Cart.find(session[:cart__id])
+    @cart = Cart.find(session[:cart_id])
   rescue ActiveRecord::RecordNotFound
     @cart = Cart.create
     session[:cart_id] = @cart.id
