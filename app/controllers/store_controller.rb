a58a87a084a8c1e785c4e89f4048855b71474ddd
alexv7 <alexvarjabedian7@gmail.com>
2016-04-29 21:25:13 -0400

    added code to get number of visits to store page

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,8 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    # The following is to get the number of visits to your homepage
+    session[:counter] ||= 0
+    @count = session[:counter] += 1
   end
 end
