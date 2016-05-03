a9c94a9adeb01016ba2ec9ed9d61b3cdbfe9dd77
alexv7 <alexvarjabedian7@gmail.com>
2016-05-02 20:42:27 -0400

    hiding an empty cart with jquery

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+ def hidden_div_if(condition, attributes = {}, &block)
+   if condition
+   attributes["style"] = "display: none"
+   end
+   content_tag("div", attributes, &block)
+ end
 end
+
+#this is what the code is essentially doing
+=begin
+<div id="cart"
+<% if @cart.line_items.empty? %>
+style="display: none"
+<% end %>
+>
+<%= render(@cart) %>
+</div>
+=end
