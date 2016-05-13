5276ed6f741d51058fd57bc2c21581c5d8c633b7
alexv7 <alexvarjabedian7@gmail.com>
2016-05-12 21:37:10 -0400

    created sessions and admin controller, added some code in their controllers and views, and added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,29 @@
 Rails.application.routes.draw do
+  # get 'admin/index'
+  #
+  # get 'sessions/new'
+  #
+  # get 'sessions/create'
+  #
+  # get 'sessions/destroy'
+
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  get "session/create"
+  get "sessions/destroy"
+
   resources :users
   resources :orders
+  resources :carts
   resources :line_items do
     put 'decrease', on: :member
     put 'increase', on: :member
   end
-  resources :carts
   get 'store/index'
 
   resources :products do
