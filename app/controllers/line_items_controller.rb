8424fe56eaa14f4b6c63ee73c8fe1b41a8b6b5fe
alexv7 <alexvarjabedian7@gmail.com>
2016-05-13 08:39:17 -0400

    limiting access to users by before_action authorize in application controller, and skip_before_action in certain controllers and actions...blacklisting all and then whitelisting some instead of blacklisting some

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,5 +1,6 @@
 class LineItemsController < ApplicationController
   include CartsHelper
+  skip_before_action :authorize, only: :create
   before_action :set_cart, only: [:create, :decrease, :increase]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_lineitem
