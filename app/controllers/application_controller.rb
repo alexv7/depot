cbb99e4f281529273739d03f8aabaef70300fb0f
alexv7 <alexvarjabedian7@gmail.com>
2016-05-13 21:08:12 -0400

    commented out the authorize callback which contained the authenticate_or_request_with_http_basic() method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,32 +6,32 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  # def authorize
-  #   unless User.find_by_id(session[:user_id]) or User.count.zero?
-  #     redirect_to login_url, :notice => "Please log in"
-  #   end
-  #   if User.count.zero?
-  #     redirect_to new_user_path, :notice => "Please create a user"
-  #   end
-  # end
-
   def authorize
-    respond_to do |format|
-      format.html do   #request.format == Mime::HTML
-        unless User.find_by(id: session[:user_id]) or User.count.zero?
-          redirect_to login_url, notice: "Please log in"
-        end
-        if User.count.zero?
-          redirect_to new_user_path, :notice => "Please create a user"
-        end
-      end
-      format.any do
-        authenticate_or_request_with_http_basic do |username, password|
-          user = User.find_by_name(username)
-          user and user.authenticate(password)
-        end
-      end
+    unless User.find_by_id(session[:user_id]) or User.count.zero?
+      redirect_to login_url, :notice => "Please log in"
+    end
+    if User.count.zero?
+      redirect_to new_user_path, :notice => "Please create a user"
     end
   end
+
+  # def authorize
+  #   respond_to do |format|
+  #     format.html do   #request.format == Mime::HTML
+  #       unless User.find_by(id: session[:user_id]) or User.count.zero?
+  #         redirect_to login_url, notice: "Please log in"
+  #       end
+  #       if User.count.zero?
+  #         redirect_to new_user_path, :notice => "Please create a user"
+  #       end
+  #     end
+  #     format.any do
+  #       authenticate_or_request_with_http_basic do |username, password|
+  #         user = User.find_by_name(username)
+  #         user and user.authenticate(password)
+  #       end
+  #     end
+  #   end
+  # end
 
 end
