fc67142ed0307f83d6120c18360c89ec223b8fce
alexv7 <alexvarjabedian7@gmail.com>
2016-05-13 20:58:19 -0400

    if the user is not logged in and the request format is not Mime::HMTL (so say for example its xml or json), instead of redirecting to the html login page, we used the authenticate_or_request_with_http_basic() in the authorize callback (see applications_controller to authenticate via popup if request type is not html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,18 +42,6 @@ class UsersController < ApplicationController
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @user.update(user_params)
-  #       format.html { redirect_to users_url, notice: "User #{@user.name} was successfully updated." }
-  #       format.json { render :show, status: :ok, location: @user }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @user.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
   def update
     @user = User.find(params[:id])
     cp = params[:user].delete('current_password')
