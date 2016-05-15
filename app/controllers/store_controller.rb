b4d9c5990f04c47eb2aedaca323e1d8bbea59180
alexv7 <alexvarjabedian7@gmail.com>
2016-05-15 13:19:48 -0400

    added a locale switcher...see layouts/application and store_controller and applicaton.css

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,7 +3,11 @@ class StoreController < ApplicationController
   skip_before_action :authorize
   before_action :set_cart
   def index
+    if params[:set_locale]
+      redirect_to store_url(locale: params[:set_locale])
+    else
     @products = Product.order(:title)
+    end
     # The following is to get the number of visits to your homepage
     session[:counter] ||= 0
     @count = session[:counter] += 1
